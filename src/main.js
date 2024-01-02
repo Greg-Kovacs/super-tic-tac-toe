1c2b953299271d54973f98f6a0a98fd2db7c9095
Greg Kovacs <kovacsgergely996@gmail.com>
2024-01-02 16:05:45 +0100

    init

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Phaser from "phaser";
+import Game from "./scenes/game";
+
+const WIDTH = 600;
+const HEIGHT = 600;
+
+const config = {
+	type: Phaser.AUTO,
+	width: WIDTH,
+	height: HEIGHT,
+};
+
+const game = new Phaser.Game(config);
+game.scene.add("game", Game);
+game.scene.start("game");
